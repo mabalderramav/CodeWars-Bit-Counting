b9cefbd0ae0d4b2d3e7ae886d1f40288b5604aca
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2016-10-18 14:31:39 -0400

    Improve BitCounting class

--- src/test/java/org/fundacionjala/codewars/bitcounting/BitCountingTest.java
+++ src/test/java/org/fundacionjala/codewars/bitcounting/BitCountingTest.java
@@ -5,15 +5,68 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by Administrator on 10/18/2016.
+ * Test for {@link BitCounting}.
  */
 public class BitCountingTest {
+
+    /**
+     * Constructor the BitCounting class.
+     */
+    public BitCountingTest() {
+    }
+
+    /**
+     * This method verify counts bits of a number.
+     */
     @Test
-    public void testGame() {
-        assertEquals(5, BitCounting.countBits(1234));
-        assertEquals(1, BitCounting.countBits(4));
-        assertEquals(3, BitCounting.countBits(7));
-        assertEquals(2, BitCounting.countBits(9));
-        assertEquals(2, BitCounting.countBits(10));
+    public void testGame1() {
+        final int expected = 5;
+        final int number = 1234;
+        final int actual = BitCounting.countBits(number);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * This method verify counts bits of a number.
+     */
+    @Test
+    public void testGame2() {
+        final int expected = 1;
+        final int number = 4;
+        final int actual = BitCounting.countBits2(number);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * This method verify counts bits of a number.
+     */
+    @Test
+    public void testGame3() {
+        final int expected = 3;
+        final int number = 7;
+        final int actual = BitCounting.countBits3(number);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * This method verify counts bits of a number.
+     */
+    @Test
+    public void testGame4() {
+        final int expected = 2;
+        final int number = 9;
+        final int actual = BitCounting.countBits(number);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * This method verify counts bits of a number.
+     */
+    @Test
+    public void testGame5() {
+        final int expected = 2;
+        final int number = 10;
+        final int actual = BitCounting.countBits2(number);
+        assertEquals(expected, actual);
     }
 }
